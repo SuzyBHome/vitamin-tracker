59aca1d5489e7907b607febc1e129896d9893b98
SuzyBHome <SuzyBHome@users.noreply.github.com>
2026-06-10 18:27:54 +1000

    Add BeeTeam logo as app icon

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'vitamins-v1';
-const ASSETS = ['./index.html', './manifest.json', './icon.svg'];
+const CACHE = 'vitamins-v2';
+const ASSETS = ['./index.html', './manifest.json', './BeeTeamVitaminLogo.png'];
 
 self.addEventListener('install', e =>
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)))
